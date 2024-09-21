32587228630d2d7a8e40453091471b9dbf81a128
Teik Guan Tan <tanteikg@gmail.com>
2024-09-21 18:28:17 +0800

    include ZKBoo project

---
+++ cli/Usage.md
@@ -0,0 +1,5 @@
+## How to run client
+
+To retrieve text key in ENS, call "node getText.mjs <ens name>"
+
+
--- cli/getText.mjs
+++ cli/getText.mjs
@@ -3,7 +3,9 @@ import { ENS } from 'web3-eth-ens';
 //const ens = new ENS(undefined,'https://mainnet.infura.io/v3/6894bcee642b497d8d9d7b0e52df192d');
 const ens = new ENS(undefined,'https://sepolia.infura.io/v3/6894bcee642b497d8d9d7b0e52df192d');
 
-console.log(await ens.getText('quantum.eth','myquantumkey'));
+var argv = process.argv.slice(2);
+
+console.log(await ens.getText(argv[0],'myquantumkey'));
 
 
 
